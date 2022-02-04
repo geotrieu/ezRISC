e31f8d006aa0aa6dedd239fe51ce2bf5d1d43ed0
Matt G <mattgarv08@gmail.com>
2022-02-04 00:34:39 -0500

    Added booth multiplier, completed alu.v

--- alu.v
+++ alu.v
@@ -5,8 +5,10 @@ parameter REG_SIZE = 32;
 input [3:0] ctrl_sig;
 input [REG_SIZE-1:0] y_data_in;
 input [REG_SIZE-1:0] bus_data_in;
-output reg [REG_SIZE-1:0] z_data_out;
+output reg [REG_SIZE + REG_SIZE -1:0] z_data_out;
+wire [REG_SIZE + REG_SIZE - 1:0] booth_data_out;
 
+booth_32x32_mult b_mult[REG_SIZE+REG_SIZE-1:0](booth_data_out, y_data_in, bus_data_in);
 // and 0000
 // or  0001
 // add 0010
@@ -29,6 +31,25 @@ begin
 		z_data_out <= (y_data_in + bus_data_in);
 	else if (ctrl_sig == 4'b0011)
 		z_data_out <= (y_data_in - bus_data_in);
-end
-
+	else if (ctrl_sig == 4'b0100)
+		z_data_out <= (y_data_in >> bus_data_in);
+	else if (ctrl_sig == 4'b0101)
+		z_data_out <= (y_data_in << bus_data_in);
+	else if (ctrl_sig == 4'b0110)
+		z_data_out <= (y_Data_in << bus_data_in | y_data_in >> $bits(y_Data_in) - bus_data_in);
+	else if (ctrl_sig == 4'b0111)
+		z_data_out <= (y_Data_in >> bus_data_in | y_data_in << $bits(y_data_in) - bus_data_in);
+		
+	else if (ctrl_sig == 4'b1000) begin
+		
+		z_data_out <= booth_data_out;
+		
+	end else if (ctrl_sig == 4'b1001)
+		z_data_out <= (y_data_in / bus_data_in);	
+		
+	else if (ctrl_sig == 4'b1010)
+		z_data_out <= (~y_data_in);	
+	else if (ctrl_sig == 4'b1011)
+		z_data_out <= (!y_data_in);	
+	end
 endmodule
