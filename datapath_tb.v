10d6cd208e318dcb22177a212a3ceb6b98ca5021
geotrieu <trieugeorge@gmail.com>
2022-02-07 14:26:26 -0500

    cleaned up code and made cleaner

--- datapath_tb.v
+++ datapath_tb.v
@@ -1,51 +1,75 @@
 // and datapath_tb.v file: <This is the filename> 
 `timescale 1ns/10ps
 module datapath_tb;
-	reg pc_out, z_low_out, mdr_out, r2_out, r4_out;           // add any other signals to see in your simulation 
-   reg mar_in, z_in, pc_in, mdr_in, ir_in, y_in;
-   reg inc_pc, read, r5_in, r2_in, r4_in;
-   reg clk, reset_n;
-   reg [31:0] m_data_in;
 
-	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
-              Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
-              T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
-   reg [3:0] Present_state = Default;
+reg pc_out, z_low_out, mdr_out, r2_out, r4_out;           // add any other signals to see in your simulation 
+reg mar_in, z_in, pc_in, mdr_in, ir_in, y_in;
+reg inc_pc, read, r5_in, r2_in, r4_in;
+reg clk, reset_n;
+reg [31:0] m_data_in;
 
-	wire [15:0] gpr_in;
-	wire [15:0] gpr_out;
-	assign gpr_in = {1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0,
-		1'b0, 1'b0, r5_in, r4_in, 1'b0, r2_in, 1'b0, 1'b0};
-	assign gpr_out = {1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0,
-		1'b0, 1'b0, 1'b0, r4_out, 1'b0, r2_out, 1'b0, 1'b0};
-		
-	wire [31:0] bus_data;
-	
-	datapath the_datapath(clk, reset_n, gpr_in, gpr_out, 1'b0, 1'b0, 1'b0, 1'b0, pc_in, pc_out, ir_in, z_in,
-		1'b0, z_low_out, y_in, mar_in, mdr_in, mdr_out, read, m_data_in,
-		bus_data);
+parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
+			  Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
+			  T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
+reg [3:0] Present_state = Default;
+
+wire [15:0] gpr_in;
+wire [15:0] gpr_out;
+assign gpr_in = {1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0,
+	1'b0, 1'b0, r5_in, r4_in, 1'b0, r2_in, 1'b0, 1'b0};
+assign gpr_out = {1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0,
+	1'b0, 1'b0, 1'b0, r4_out, 1'b0, r2_out, 1'b0, 1'b0};
+
+wire [3:0] alu_op;
+assign alu_op = 4'b0001; // AND Operation
+
+wire [31:0] bus_data;
+
+datapath the_datapath(
+	.clk(clk),
+	.reset_n(reset_n),
+	.gpr_in(gpr_in),
+	.gpr_out(gpr_out),
+	.hi_in(1'b0),
+	.hi_out(1'b0),
+	.lo_in(1'b0),
+	.lo_out(1'b0),
+	.pc_in(pc_in),
+	.pc_out(pc_out),
+	.ir_in(ir_in),
+	.z_in(z_in),
+	.z_high_out(1'b0),
+	.z_low_out(z_low_out),
+	.y_in(y_in),
+	.mar_in(mar_in),
+	.mdr_in(mdr_in),
+	.mdr_out(mdr_out),
+	.read(read),
+	.m_data_in(m_data_in),
+	.alu_op(alu_op),
+	.bus_data(bus_data));
  
-	initial
-	begin
-		clk = 0;
-		forever #10 clk = ~clk;
-	end
+initial
+begin
+	clk = 0;
+	forever #10 clk = ~clk;
+end
 
 always @(posedge clk)  // finite state machine; if clock rising-edge
 begin
 	case (Present_state)
-		Default: #40 Present_state = Reg_load1a;
+		Default: 	#40 Present_state = Reg_load1a;
 		Reg_load1a: #40 Present_state = Reg_load1b;
 		Reg_load1b: #40 Present_state = Reg_load2a;
 		Reg_load2a: #40 Present_state = Reg_load2b;
 		Reg_load2b: #40 Present_state = Reg_load3a;
 		Reg_load3a: #40 Present_state = Reg_load3b;
 		Reg_load3b: #40 Present_state = T0;
-		T0: #40 Present_state = T1;
-		T1: #40 Present_state = T2;
-		T2: #40 Present_state = T3;
-		T3: #40 Present_state = T4;
-		T4: #40 Present_state = T5;
+		T0: 			#40 Present_state = T1;
+		T1: 			#40 Present_state = T2;
+		T2: 			#40 Present_state = T3;
+		T3: 			#40 Present_state = T4;
+		T4: 			#40 Present_state = T5;
    endcase
 end
                                                
@@ -103,36 +127,28 @@ begin
          #15 mdr_out <= 0; r5_in <= 0;  // initialize R5 with the value $26
 		end
 		T0: begin  // see if you need to de-assert these signals 
-			pc_out <= 1;
-			mar_in <= 1;
-			inc_pc <= 1;
-			#20 pc_out <= 0; mar_in <= 0; inc_pc <= 0;
+			#10 pc_out <= 1; mar_in <= 1; inc_pc <= 1;
+			#15 pc_out <= 0; mar_in <= 0; inc_pc <= 0;
 		end
 		T1: begin 
-			read <= 1;
-			mdr_in <= 1;
-			m_data_in <= 32'h4A920000;       // opcode for “and R5, R2, R4”
-			#20 read <= 0; mdr_in <= 0;
+			#10 read <= 1; mdr_in <= 1; m_data_in <= 32'h4A920000;       // opcode for “and R5, R2, R4”
+			#15 read <= 0; mdr_in <= 0;
 		end
 		T2: begin
-			mdr_out <= 1;
-			ir_in <= 1;
-			#20 mdr_out <= 0; ir_in <= 0;
+			#10 mdr_out <= 1; ir_in <= 1;
+			#15 mdr_out <= 0; ir_in <= 0;
 		end
 		T3: begin
-			r2_out <= 1;
-			y_in <= 1;
-			#20 r2_out <= 0; y_in <= 0;
+			#10 r2_out <= 1; y_in <= 1;
+			#15 r2_out <= 0; y_in <= 0;
 		end
 		T4: begin
-			r4_out <= 1;
-			z_in <= 1;
-			#20 r4_out <= 0; z_in <= 0;
+			#10 r4_out <= 1; z_in <= 1;
+			#15 r4_out <= 0; z_in <= 0;
 		end
 		T5: begin
-			z_low_out <= 1;
-			r5_in <= 1;
-			#20 z_low_out <= 0; r5_in <= 0;
+			#10 z_low_out <= 1; r5_in <= 1;
+			#15 z_low_out <= 0; r5_in <= 0;
 		end
 	endcase
 end
--- mdr.v
+++ mdr.v
@@ -10,9 +10,15 @@ input [REG_SIZE-1:0] bus_mux_out;
 input [REG_SIZE-1:0] m_data_in;
 output [REG_SIZE-1:0] mdr_output;
 
-wire [REG_SIZE-1:0] mdr_input;
+wire [REG_SIZE-1:0] mdr_input; // intermediate signal to connect md_mux with mdr_reg's d input
 
-mux_32bit_2to1 md_mux(bus_mux_out, m_data_in, md_mux_select, mdr_input);
+// Choose between bus_mux_out (0) and m_data_in (1) to be D input to MDR
+mux_32bit_2to1 md_mux(
+	.a(bus_mux_out),
+	.b(m_data_in),
+	.sel(md_mux_select),
+	.out(mdr_input));
+// MDR Register
 gp_register mdr_reg(clk, reset_n, mdr_in, mdr_input, mdr_output);
 
 endmodule
