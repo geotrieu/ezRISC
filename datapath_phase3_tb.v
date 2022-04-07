bfcc4b558b9bcb5d7ae3144d9ee007fedb3f2551
George Trieu <geocrafterserver@gmail.com>
2022-04-07 17:24:24 -0400

    added control signals and fixed RAM contents

--- datapath_phase3_tb.v
+++ datapath_phase3_tb.v
@@ -5,10 +5,15 @@ module datapath_phase3_tb;
 reg fast_clk, reset_n;
 wire [7:0] seven_seg_out_1;
 wire [7:0] seven_seg_out_2;
+wire stop;
+assign stop = 0;
+wire run;
 datapath_phase3 the_datapath(
 	.fast_clk(fast_clk),
 	.reset_n(reset_n),
-	.inport_ext_input(32'd0),
+	.stop(stop),
+	.run(run),
+	.inport_ext_input(32'h88),
 	.seven_seg_out_1(seven_seg_out_1),
 	.seven_seg_out_2(seven_seg_out_2));
  
